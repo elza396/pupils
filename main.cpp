31c9bbf76262f91506739af303c88c08f9914110
elza396 <elza396@yandex.ru>
2021-03-03 16:15:44 +0500

    Тема: классы, конструкторы Про работников

--- main.cpp
+++ main.cpp
@@ -2,34 +2,57 @@
 using namespace std;
 
 int main() {
-    int N;
-    cout << "Enter number of pupils:" << "\t";
-    cin >> N;
+    class Employee {
+        private:
+        string name;
+        int category, salary;
 
-    struct Pupil {
-        string surname;
-        int school = 0;
-        int grade = 1;
-    };
-    Pupil pupils[N];
-
-    for (int i = 0; i < N; i++) {
-        Pupil pupil;
-        cout << "Pupil "<< i+1 << "'s name: " << "\t";
-        cin >> pupil.surname;
-        cout << "Enter school: " << "\t";
-        cin >> pupil.school;
-        cout << "Enter grade: " << "\t";
-        cin >> pupil.grade;
-
-        pupils[i] = pupil;
-    }
-
-    cout <<endl<<"\t"<<"Pupils in 49 or 90 schools in 5 or 6 grades: ";
-    for (int i = 0; i < N; i++) {
-        if ((pupils[i].school == 49 || pupils[i].school == 90) && (pupils[i].grade == 5 || pupils[i].grade == 6)) {
-            cout <<endl<< pupils[i].surname;
+        public:
+        int getSalary() {
+            return salary;
         }
+
+        string getName() {
+            return name;
+        }
+
+        void setCategory(int employeeCategory) {
+            category = employeeCategory;
+            salary = category * 7000;
+        }
+
+        Employee(string employeeName) {
+            name = employeeName;
+            setCategory(1);
+        }
+
+        Employee(string employeeName, int employeeCategory) {
+            name = employeeName;
+            setCategory(employeeCategory);
+        }
+    };
+    string name1, name2;
+    int category;
+
+    cout << "Enter employee's name:" <<"\t";
+    cin >> name1;
+    cout << "Enter employee's category:" <<"\t";
+    cin >> category;
+
+    if (category < 1) {
+        cout <<"Invalid category";
+    } else {
+        Employee employee1(name1, category);
+        cout << "Employee " << employee1.getName() << " gets " << employee1.getSalary() <<" rubles."<<"\t";
     }
+
+    cout << endl <<"Enter trainee's name:" <<"\t";
+    cin >> name2;
+    Employee employee2(name2);
+    cout << "Trainee " << employee2.getName() << " gets " << employee2.getSalary() <<" rubles."<<"\t";
+
+
+
     return 0;
 }
+
