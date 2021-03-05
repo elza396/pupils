79cb8b3811a8a8dec20d81af7b21509a03345fc5
elza396 <elza396@yandex.ru>
2021-03-05 17:15:15 +0500

    Тема: классы, конструкторы, наследование

--- main.cpp
+++ main.cpp
@@ -1,45 +1,45 @@
 #include <iostream>
 using namespace std;
 
+class BaseSalary {
+protected:
+    int baseSalary;
+    string position;
+public:
+    BaseSalary(string newPosition, int salary) {
+        baseSalary = salary;
+        position = newPosition;
+    }
+    void showBaseSalary() {
+        cout << "Position: " << position;
+        cout << endl << "Base salary: " << baseSalary << " rubles" << endl;
+    }
+};
+
+class Salary: public BaseSalary {
+private:
+    int salary, bonus, interest;
+public:
+    Salary(string newPosition, int newSalary, int newBonus, int newInterest) : BaseSalary(newPosition, newSalary) {
+        bonus = newBonus;
+        interest = newInterest;
+        calcSalary();
+    }
+
+    int calcSalary() {
+        salary = (baseSalary + bonus) - (baseSalary + bonus) * interest / 100;
+    }
+
+    void showSalary() {
+        cout << endl << "Position: " << position;
+        cout << endl << "Salary: " << salary << " rubles" <<endl;
+    }
+};
+
 int main() {
-    class Planet {
-    public:
-        float weight;
-        string name;
-
-        Planet(string planetName, float planetWeight) {
-            name = planetName;
-            weight =  planetWeight;
-        }
-    };
-
-    const int n = 8;
-
-    Planet planets[n] = {
-            Planet("Земля", 5.972E24),
-            Planet("Уран", 8.681E25),
-            Planet("Венера", 4.867E24),
-            Planet("Нептун", 1.024E26),
-            Planet("Марс", 6.39E23),
-            Planet("Юпитер", 1.898E27),
-            Planet("Меркурий", 3.285E23),
-            Planet("Сатурн", 5.683E26),
-    };
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1 - i; j++) {
-            if(planets[j].weight > planets[j + 1].weight) {
-                Planet temp =  planets[j];
-                planets[j] = planets[j + 1];
-                planets[j + 1] = temp;
-            }
-        }
-    }
-
-    cout << "Планеты солнечной системы (отсортированные):" << endl;
-    for (int k = 0; k < n; k++) {
-        cout << planets[k].name << "\t" << planets[k].weight <<endl;
-    }
+    Salary ivan("developer", 30000, 10000, 13);
+    ivan.showBaseSalary();
+    ivan.showSalary();
 
     return 0;
 }
